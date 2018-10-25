ee683a1fa80b5401cbfdb2ccda207fdb8061137d
Martijn van Buul <jintarmlubu@gmail.com>
2018-10-25 21:50:31 +0200

    Added a TileManager which controls the tiles that the player owns.

---
+++ Assets/Scripts/Managers/TileManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public static TileManager instance;
+
+    public int InitialWidth = 11;
+    public int InitialDepth = 11;
+
+    public Vector3 InitialPosition;
+
+    public List<GameObject> TilePrefabs;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start () {
+        InitializeGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void InitializeGrid()
+    {
+        for(int i = -InitialWidth / 2; i <= InitialWidth / 2; i++)
+        {
+            for (int j = -InitialDepth / 2; j <= InitialDepth / 2; j++)
+            {
+                Instantiate(TilePrefabs[0], new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
+    }
+}
